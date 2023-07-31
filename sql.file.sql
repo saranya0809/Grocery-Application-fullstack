151f2633d7c39485dc786f04f29a49c15b7e1934
saranya0809 <103360689+saranya0809@users.noreply.github.com>
2023-07-31 14:08:26 +0530

    Add files via upload

---
+++ sql.file.sql
@@ -0,0 +1,21 @@
+SELECT * FROM groceryapplication.products;
+
+use groceryapplication;
+ALTER TABLE products modify image varchar(4000);
+insert into products values(1,'2023-07-20', 'Fortune Sunflower oil is a light, healthy and nutritious cooking oil','https://m.media-amazon.com/images/I/412TbDp8+mL._AC_UL480_FMwebp_QL65_.jpg', 'Fortune Sunlite Refined Sunflower oil', 650);
+update products set image ='https://m.media-amazon.com/images/I/41tfQG9ggIL._SY300_SX300_QL70_FMwebp_.jpg' where id=1;
+update products set name ='Fresh Grapefruit 1kg' where id=1;
+update products set price ='250' where id=1;
+update products set description='High in vitamin C.' where id=1;
+insert into products values(2,'2023-07-21', 'It has many white layers on its inside which have a strong, sharp smell and taste','https://m.media-amazon.com/images/I/61E-eLMEkjL._SL1500_.jpg', 'Onion 1kg', 60);
+insert into products values(3,'2023-07-23', 'Roundnuts are abundant in protein, healthy fats and dietary fibre.','https://m.media-amazon.com/images/I/71uQOA2YGcL._SL1500_.jpg', 'Raw Peanut 500g (Loose)', 171 );
+insert into products values(4,'2023-07-24', 'Sugar is a form of carbohydrate and it provides the same amount of energy or kilojoules (kJ) per gram.','https://m.media-amazon.com/images/I/51iL74dA4yS.jpg', 'Fresh Produce More Sugar Loose 1kg', 50 );
+insert into products values(5,'2023-07-25', 'Honey is a sweet fluid made by honeybees using the nectar of flowering plants.','https://cpimg.tistatic.com/07554554/b/4/Sweet-Honey.jpg', 'Sweet Honey', 100 );
+insert into products values(6,'2023-07-26', 'It is frequently used in medicines, food, and cosmetics across the globe..','https://m.media-amazon.com/images/I/61SCxbFGpWL._SL1500_.jpg', 'Fresh Ginger, 100g', 50 );
+insert into products values(7,'2023-07-27', 'Remove Tough Stains | Surf Excel Front-Load Washing Powder .','https://media.naheed.pk/catalog/product/cache/49dcd5d85f0fa4d590e132d0368d8132/1/1/1118932-1.jpg', 'Surf Exel, 1kg', 150 );
+insert into products values(8,'2023-07-28', 'Fortune Sunflower oil is a light, healthy and nutritious cooking oil .','https://m.media-amazon.com/images/I/51TH5xE+2sL.jpg', 'Fortune Sunlite Refined Sunflower Oil, 1L', 150 );
+insert into products values(9,'2023-07-29', ' Protects the heart. Rich in antioxidants, tomatoes help to maintain healthy blood vessels..','http://purepng.com/public/uploads/large/purepng.com-red-tomatoestomatosalad-fruitred-fruittomatoes-1701527317523ofag1.png', 'Tomato, 1kg', 170 );
+update products set image='https://www.feedipedia.org/sites/default/files/images/tomato_fruits.jpg'  where id='9';
+update products set name='Surf Excel'  where id='7';
+use groceryapplication;
+update products set name= "Fresh orange 1kg" where id=1;
